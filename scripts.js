0517a735078ef21971471c26a257182019100f65
Wheaties466 <Mwheat42289@gmail.com>
2023-12-10 09:13:47 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -135,28 +135,27 @@ function makeStreamsDraggable() {
         },
         stop: function() {
             $(this).removeClass('dragging');
-        }
+        },
+        grid: [10, 10]
     });
 
     $('.stream-container').droppable({
         accept: '.stream',
+        tolerance: 'intersect',
         drop: function(event, ui) {
             var draggedElement = ui.draggable;
-            var dropTargetIndex = $(event.target).children().index(ui.helper);
-            handleStreamDrop(draggedElement, dropTargetIndex);
+            var targetIndex = $(this).children().index(ui.helper);
+            var draggedIndex = draggedElement.index();
+
+            if (draggedIndex < targetIndex) {
+                $(this).children().eq(targetIndex).after(draggedElement);
+            } else {
+                $(this).children().eq(targetIndex).before(draggedElement);
+            }
+
             saveStreamOrder();
         }
     });
-}
-
-// Handle stream drop
-function handleStreamDrop(draggedElement, dropTargetIndex) {
-    var allStreams = $('#live-streams').children('.stream');
-    if (dropTargetIndex >= allStreams.length) {
-        $('#live-streams').append(draggedElement);
-    } else {
-        $(allStreams[dropTargetIndex]).before(draggedElement.detach());
-    }
 }
 
 // Save stream order to localStorage
